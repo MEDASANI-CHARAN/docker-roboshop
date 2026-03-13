a40a7019f41cec654cf1cfa5c30811d0cd62d4da
M CHARAN <charanchinna646@gmail.com>
2026-03-13 23:38:52 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "docker" {
   ami           = var.ami_id
   # instance_type = each.value
   instance_type = "t3.micro"
-  user_data = file("docker.sh")    
+  user_data = file("docker.sh")               
                                               
   vpc_security_group_ids = [aws_security_group.allow_everything.id]
               
